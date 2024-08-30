fe69f2643e126e99c36b96133349b0238af3ed29
Yuto Tsuneda <yuto.tsuneda@prum.com>
2024-08-30 21:42:11 +0900

    favicon setting

--- assets/header.php
+++ assets/header.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0, minimum-scale=1.0">
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
   <title><?php bloginfo('name'); ?></title>
   <?php wp_head(); ?>
 </head>
